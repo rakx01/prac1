5240f8d8743bd4d5ed80652d63a741bb62152c0f
rakx01 <zhenechka.rak@mail.ru>
2020-02-24 10:22:31 +0300

    version3

---
+++ prac1/Monitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prac1
+{
+    class Monitor
+    {
+
+        public string Firm { get; set; }
+        public string Type { get; set; }
+        public float Diagonal { get; set; }
+        public void Mon()
+        {
+            Console.WriteLine("Фирма:" + Firm + " " + "Тип:" + Type + " " + "Диагональ:" + Diagonal + " ");
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите фирму: ");
+            Firm = Console.ReadLine();
+            Console.WriteLine("Введите тип: ");
+            Type = Console.ReadLine();
+            Console.WriteLine("Введите диагональ: ");
+            Diagonal = int.Parse(Console.ReadLine());
+        }
+    }
+}
--- prac1/Program.cs
+++ prac1/Program.cs
@@ -2,36 +2,16 @@
 
 namespace prac1
 {
-    class Monitor
-    {
-        public string Firm { get; set; }
-        public string Type { get; set; }
-        public float Diagonal { get; set; }
-
-        public void print()
+        class Program
         {
-            Console.WriteLine("Фирма:" + Firm + " " + "Тип:" + Type + " " + "Диагональ:" + Diagonal + " ");
-        }
-
-        public void vrite()
-        {
-            Console.WriteLine("Введите фирму: ");
-            Firm = Console.ReadLine();
-            Console.WriteLine("Введите тип: ");
-            Type = Console.ReadLine();
-            Console.WriteLine("Введите диагональ: ");
-            Diagonal = int.Parse(Console.ReadLine());
+            static void Main(string[] args)
+            {
+                Monitor name = new Monitor();
+                name.Input();
+                name.Mon();
+                Console.ReadKey();
+            }
         }
     }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Monitor name = new Monitor();
-            name.vrite();
-            name.print();
-            Console.ReadKey();
-        }
-    }
-}
+
