058737da2bdab69613e8aab2dbc59d7f77bd29db
rakx01 <zhenechka.rak@mail.ru>
2020-02-24 09:36:32 +0300

    version2

--- prac1/Program.cs
+++ prac1/Program.cs
@@ -2,12 +2,36 @@
 
 namespace prac1
 {
+    class Monitor
+    {
+        public string Firm { get; set; }
+        public string Type { get; set; }
+        public float Diagonal { get; set; }
+
+        public void print()
+        {
+            Console.WriteLine("Фирма:" + Firm + " " + "Тип:" + Type + " " + "Диагональ:" + Diagonal + " ");
+        }
+
+        public void vrite()
+        {
+            Console.WriteLine("Введите фирму: ");
+            Firm = Console.ReadLine();
+            Console.WriteLine("Введите тип: ");
+            Type = Console.ReadLine();
+            Console.WriteLine("Введите диагональ: ");
+            Diagonal = int.Parse(Console.ReadLine());
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            Monitor name = new Monitor();
+            name.vrite();
+            name.print();
+            Console.ReadKey();
         }
     }
 }
